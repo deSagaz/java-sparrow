eb29d010fdbd1a82a4c32514a9c64cc58c965aef
Ivar Troost <ivar@desagaz.com>
2017-10-12 13:06:15 +0200

    Add permissive CORS (Cross-Origin Resource Sharing) headers

--- javasparrow-server/javasparrow/settings.py
+++ javasparrow-server/javasparrow/settings.py
@@ -61,11 +61,13 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
 
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
