d52106ac2bb944f0a4d9bfbf0357be2abda68b7d
Ivar Troost <ivar@desagaz.com>
2017-10-13 12:51:28 +0200

    Remove side menu from app

--- javasparrow-client/src/app/app.component.ts
+++ javasparrow-client/src/app/app.component.ts
@@ -8,22 +8,22 @@ import { FirstRunPage } from '../pages/pages';
 import { Settings } from '../providers/providers';
 
 @Component({
-  template: `<ion-menu [content]="content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Pages</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  template: `<!--<ion-menu [content]="content">-->
+    <!--<ion-header>-->
+      <!--<ion-toolbar>-->
+        <!--<ion-title>Pages</ion-title>-->
+      <!--</ion-toolbar>-->
+    <!--</ion-header>-->
 
-    <ion-content>
-      <ion-list>
-        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
-          {{p.title}}
-        </button>
-      </ion-list>
-    </ion-content>
+    <!--<ion-content>-->
+      <!--<ion-list>-->
+        <!--<button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">-->
+          <!--{{p.title}}-->
+        <!--</button>-->
+      <!--</ion-list>-->
+    <!--</ion-content>-->
 
-  </ion-menu>
+  <!--</ion-menu>-->
   <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
@@ -44,7 +44,7 @@ export class MyApp {
     { title: 'Menu', component: 'MenuPage' },
     { title: 'Settings', component: 'SettingsPage' },
     { title: 'Search', component: 'SearchPage' }
-  ]
+  ];
 
   constructor(private translate: TranslateService, private platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
     this.initTranslate();
