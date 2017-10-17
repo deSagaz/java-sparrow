948f11bdca97bdd614e05d819e23a4c7ff9bfb24
Ivar Troost <ivar@desagaz.com>
2017-10-17 18:49:24 +0200

    Further implement internal game progression

--- javasparrow-client/src/pages/seq/seq.ts
+++ javasparrow-client/src/pages/seq/seq.ts
@@ -17,29 +17,79 @@ export class SeqPage {
 
   api: string = '{"events":[{"id":5,"eventType":"text","data":{"content":"Welcome Agent Sparrow, always an honour to have such a talented spy in my office."}},{"id":23,"eventType":"backgroundChange","data":{"image":"https://example.com/image.jpg"}},{"id":20,"eventType":"quiz","data":{"question":"What is a JavaScript array?","answer1":"A dedicated data type","answer2":"An regular JavaScript object","rightAnswerResponse":"Maybe I should be in that big chair!","rightAnswerResponseColor":"purple","wrongAnswer":"Woops, perhaps Javascript is a bit weird after all..."}},{"id":1,"eventType":"quiz","data":{"question":"What is a Javascript array?","answer_1":"A dedicated data type","answer_2":"An regular object"}}]}';
   sequence: object[];
-  currentEvent: number;
+  currentEventIndex: number;
+  currentEventType: string;
+
+  // primaryText is used for character dialogue and question texts
+  primaryText: string;
+
+  // Which components to show?
+  showPrimaryText: boolean = true;
+  showMultipleChoice: boolean = false;
+  showNextButton: boolean = true;
+
+
 
   init(sequenceObject: object) {
     this.sequence = sequenceObject['events'];
-    this.currentEvent = -1;
+    this.currentEventIndex = -1;
   }
 
   next() {
-    this.currentEvent++;
-    return this.sequence[this.currentEvent];
+    console.log("NEXT EVENT TRIGGERED: #" + (this.currentEventIndex + 1)); // DEBUG
+
+    if(this.currentEventIndex == this.sequence.length - 1) {
+      // sequence has ended. Return to chapter menu screen and check off level for user.
+    }
+    this.currentEventIndex++;
+    this.currentEventType = this.sequence[this.currentEventIndex]['eventType'];
+
+    // Now decide what to do with this event type
+    if (this.currentEventType == "text") {
+      this.doText();
+    } else if (this.currentEventType == "backgroundChange") {
+      this.doBackgroundChange();
+    } else if (this.currentEventType == "quiz") {
+      this.doQuiz();
+    }
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.init(JSON.parse(this.api));
-    this.gameLoop();
-  }
-
-  gameLoop() {
-    while(this.currentEvent < this.sequence.length - 1) {
-      console.log(this.next());
-    }
+    this.next();
   }
 
   ionViewDidLoad() {
   }
+
+
+  doText() {
+    // Load data
+    this.primaryText = this.sequence[this.currentEventIndex]['data']['content'];
+
+    // Set interface
+    this.showPrimaryText = true;
+    this.showMultipleChoice = false;
+    this.showNextButton = true;
+  }
+
+  doBackgroundChange() {
+    // Change background
+
+    // Immediately run next event
+    this.next();
+  }
+
+  doQuiz() {
+    // Load data
+    this.primaryText = this.sequence[this.currentEventIndex]['data']['question'];
+    // TODO: Load questions into component
+
+    // Set interface
+    this.showPrimaryText = true;
+    this.showMultipleChoice = true;
+    this.showNextButton = false;
+  }
+
+
 }
