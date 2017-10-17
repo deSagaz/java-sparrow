845174aeca44b9af2292574a14a685c14b788184
Ivar Troost <ivar@desagaz.com>
2017-10-17 20:30:33 +0200

    Adjust settings to account for new database version

--- javasparrow-server/javasparrow/settings.py
+++ javasparrow-server/javasparrow/settings.py
@@ -115,10 +115,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sparrow',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'sparrow',
+        'PASSWORD': 'sparrow',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
