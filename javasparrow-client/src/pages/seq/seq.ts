7e08f8e76cf48bc3026bc25ca850e79e6fae99c6
Ivar Troost <ivar@desagaz.com>
2017-10-17 18:22:28 +0200

    Implement basic game loop

--- javasparrow-client/src/pages/seq/seq.ts
+++ javasparrow-client/src/pages/seq/seq.ts
@@ -16,16 +16,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SeqPage {
 
   api: string = '{"events":[{"id":5,"eventType":"text","data":{"content":"Welcome Agent Sparrow, always an honour to have such a talented spy in my office."}},{"id":23,"eventType":"backgroundChange","data":{"image":"https://example.com/image.jpg"}},{"id":20,"eventType":"quiz","data":{"question":"What is a JavaScript array?","answer1":"A dedicated data type","answer2":"An regular JavaScript object","rightAnswerResponse":"Maybe I should be in that big chair!","rightAnswerResponseColor":"purple","wrongAnswer":"Woops, perhaps Javascript is a bit weird after all..."}},{"id":1,"eventType":"quiz","data":{"question":"What is a Javascript array?","answer_1":"A dedicated data type","answer_2":"An regular object"}}]}';
+  sequence: object[];
+  currentEvent: number;
 
-  sequence: object;
+  init(sequenceObject: object) {
+    this.sequence = sequenceObject['events'];
+    this.currentEvent = -1;
+  }
+
+  next() {
+    this.currentEvent++;
+    return this.sequence[this.currentEvent];
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.sequence = JSON.parse(this.api);
-    console.log(this.sequence);
+    this.init(JSON.parse(this.api));
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    while(this.currentEvent < this.sequence.length - 1) {
+      console.log(this.next());
+    }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SeqPage');
   }
-
 }
