611edbf2fae5626881be46c735dbf89883aa2acb
Ivar Troost <ivar@desagaz.com>
2017-10-17 19:00:12 +0200

    Implement background change event

--- javasparrow-client/src/pages/seq/seq.ts
+++ javasparrow-client/src/pages/seq/seq.ts
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SeqPage {
 
-  api: string = '{"events":[{"id":5,"eventType":"text","data":{"content":"Welcome Agent Sparrow, always an honour to have such a talented spy in my office."}},{"id":23,"eventType":"backgroundChange","data":{"image":"https://example.com/image.jpg"}},{"id":20,"eventType":"quiz","data":{"question":"What is a JavaScript array?","answer1":"A dedicated data type","answer2":"An regular JavaScript object","rightAnswerResponse":"Maybe I should be in that big chair!","rightAnswerResponseColor":"purple","wrongAnswer":"Woops, perhaps Javascript is a bit weird after all..."}},{"id":1,"eventType":"quiz","data":{"question":"What is a Javascript array?","answer_1":"A dedicated data type","answer_2":"An regular object"}}]}';
+  api: string = '{"events":[{"id":5,"eventType":"text","data":{"content":"Welcome Agent Sparrow, always an honour to have such a talented spy in my office."}},{"id":23,"eventType":"backgroundChange","data":{"image":"assets/img/tech4learn_duckSlam.jpg"}},{"id":20,"eventType":"quiz","data":{"question":"What is a JavaScript array?","answer1":"A dedicated data type","answer2":"An regular JavaScript object","rightAnswerResponse":"Maybe I should be in that big chair!","rightAnswerResponseColor":"purple","wrongAnswer":"Woops, perhaps Javascript is a bit weird after all..."}},{"id":1,"eventType":"quiz","data":{"question":"What is a Javascript array?","answer_1":"A dedicated data type","answer_2":"An regular object"}}]}';
   sequence: object[];
   currentEventIndex: number;
   currentEventType: string;
@@ -27,6 +27,9 @@ export class SeqPage {
   showPrimaryText: boolean = true;
   showMultipleChoice: boolean = false;
   showNextButton: boolean = true;
+
+  // Background
+  backgroundImage: string = "/assets/img/duckAtDesk.jpg";
 
 
 
@@ -75,7 +78,7 @@ export class SeqPage {
 
   doBackgroundChange() {
     // Change background
-
+    this.backgroundImage = this.primaryText = this.sequence[this.currentEventIndex]['data']['image'];
     // Immediately run next event
     this.next();
   }
